1bccecf8cdca591d2560b42b4541e81cbe618bb4
ab-2109 <arhambhansali2109@kgpian.iitkgp.ac.in>
2025-06-21 21:35:10 +0530

    changes in chat.js

--- frontends/public/static/js/chat.js
+++ frontends/public/static/js/chat.js
@@ -2,7 +2,7 @@
 let waitingForClarification = false;
 let activeSessionId = null;
 
-const API_BASE = '/api'; 
+const API_BASE = 'https://3.86.52.25.nip.io'; 
 const DEBUG = true;
 
 // Debug function
