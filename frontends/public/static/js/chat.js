47821d7f3dd9e2c1b95e3578015f35bd89b5e986
ab-2109 <arhambhansali2109@kgpian.iitkgp.ac.in>
2025-06-21 20:07:27 +0530

    changes in nextconfig.js

--- frontends/public/static/js/chat.js
+++ frontends/public/static/js/chat.js
@@ -2,7 +2,7 @@
 let waitingForClarification = false;
 let activeSessionId = null;
 
-const API_BASE = 'https://3.86.52.25.nip.io'; 
+const API_BASE = '/api'; 
 const DEBUG = true;
 
 // Debug function
