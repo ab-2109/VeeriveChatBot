cefb25f4528a8b38ae63af0ceba95ad73f593478
ab-2109 <arhambhansali2109@kgpian.iitkgp.ac.in>
2025-09-15 15:49:17 +0530

    bug in qdrant api

--- prompt_guidance.py
+++ prompt_guidance.py
@@ -26,7 +26,7 @@ class QdrantConfig:
 
     
     QDRANT_URL = os.getenv("QDRANT_URL")
-    QDRANT_API_KEY = os.getenv("QDRANT_API_KEY")
+    QDRANT_API_KEY = os.getenv("QDRANT_API")
     QDRANT_COLLECTION = "prompt-guidance"
     OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
     EMBEDDING_MODEL = "text-embedding-3-large"
